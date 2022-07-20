ddd8bc9a7665f58b6d48dc4a91be272e8031c52c
ivanvit <84175732+ivanvit100@users.noreply.github.com>
2022-07-20 10:21:53 +0300

    Pip test

--- script/1script.js
+++ script/1script.js
@@ -1,3 +1,10 @@
+video = document.getElementById("video");
+checkbox = document.querySelector(".checkbox");
+state = document.getElementById("state-s");
+progress = document.querySelector(".progress");
+knobs = document.querySelector(".knobs");
+layer = document.querySelector(".layer");
+
 function togglePictureInPicture(){
 	if(document.pictureInPictureElement){
 		document.exitPictureInPicture();
@@ -14,37 +21,29 @@ function togglePictureInPicture(){
 	}
 }
 
+ez_consent.init({
+	is_always_visible: true,
+	privacy_url: 'https://ivanvit.ru/',
+	texts: {
+		main: "Режим отображения 'Картинка в картинке' работает не во всех браузерах",
+		buttons: {
+			ok: "Ок",
+			more: "Связаться"
+		}
+	}
+});
+
 window.onload = function(){
 	try{
-		video = document.getElementById("video");
-		checkbox = document.querySelector(".checkbox");
-		state = document.getElementById("state-s");
-		progress = document.querySelector(".progress");
-		knobs = document.querySelector(".knobs");
-		layer = document.querySelector(".layer");
-
 		navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function(stream){
 			video.srcObject = stream;
 			video.play();
 			state.innerHTML = "Доступ к камере получен";
 			progress.style.border = "3px solid #ff7f26";
-		});
-
-		video.addEventListener("click", togglePictureInPicture);
-
-		ez_consent.init({
-			is_always_visible: true,
-			privacy_url: 'https://ivanvit.ru/',
-			texts: {
-				main: "Режим отображения 'Картинка в картинке' работает не во всех браузерах",
-				buttons: {
-					ok: "Ок",
-					more: "Связаться"
-				}
-			}
+			video.addEventListener("click", togglePictureInPicture);		
 		});
 	}
 	catch(err){
-		document.location.reload();
+		console.log(err);
 	}
 };
