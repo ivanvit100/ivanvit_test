814b415bdb9bf15b6a5968e71a4c034064782d41
ivanvit <84175732+ivanvit100@users.noreply.github.com>
2022-07-21 14:48:55 +0300

    Download change 2

--- script/shoot.js
+++ script/shoot.js
@@ -1,3 +1,13 @@
+var download = function(href, name){
+	var link = document.createElement('a');
+	link.download = name;
+	link.style.display = "none";
+	document.append(link);
+	link.href = href;
+	link.click();
+	link.remove();
+}
+
 document.getElementById("shoot").onclick = function(){
 	video = document.querySelector("#video");
 	png = document.querySelector("#png").checked;
@@ -9,33 +19,19 @@ document.getElementById("shoot").onclick = function(){
 		filetype = "jpg";
 	}
 	else{
-		filetype = "svg";
+		filetype = "webp";
 	}
 	canvas = window.canvas = document.createElement("canvas");
 	canvas.width = video.videoWidth;
 	canvas.height = video.videoHeight;
-	canvas.getContext ('2d'). drawImage (video, 0, 0, canvas.width, canvas.height);
-	strDataURL = canvas.toDataURL ("image/" + filetype);
-	var arr = strDataURL.split(','),
-			mime = arr[0].match(/:(.*?);/)[1],
-			bstr = atob(arr[1]),
-			n = bstr.length,
-			u8arr = new Uint8Array(n);
-		while (n--) {
-			u8arr[n] = bstr.charCodeAt(n);
-		}
-		var blob = new Blob([u8arr], {
-			type: mime
-		});
-	url = window.URL.createObjectURL(blob);
-	var a = document.createElement('a');  
-		a.style.display = 'none';
-		a.href = url;
-		a.download = "Photo." + filetype;
-		document.body.appendChild(a);
-		a.click();
-		setTimeout(function () {
-			document.body.removeChild(a);
-			window.URL.revokeObjectURL(url);
-		}, 1000);
+	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+	strDataURL = canvas.toDataURL("image/" + filetype);
+	a = document.createElement('a');  
+	a.style.display = 'none';
+	a.href = strDataURL;
+	a.download = "Photo." + filetype;
+	document.body.appendChild(a);
+	a.click();
+	document.body.removeChild(a);
+	window.URL.revokeObjectURL(strDataURL);
 }
